9518248c9a1330c1eedc5054d56055e5ad97f74e
Abdelgawad Hamdy <34074927+abdelgawadhamdy2015@users.noreply.github.com>
2026-09-10 15:25:22 +0300

    feat(attendance-reports): add employee report repository API

--- lib/features/attendance/domain/repositories/attendance_reports_repository.dart
+++ lib/features/attendance/domain/repositories/attendance_reports_repository.dart
@@ -2,29 +2,11 @@ import '../../../../core/utils/result.dart';
 import '../../data/models/attendance_report_models.dart';
 
 abstract class AttendanceReportsRepository {
-  Future<Result<AttendanceReportModel>> getReport(
-    AttendanceReportRequestModel request,
-  );
-
-  Future<Result<AttendanceReportModel>> getDailyReport({
-    required DateTime date,
-    int? employeeId,
-    String? department,
-  });
-
-  Future<Result<AttendanceReportModel>> getLateReport(
-    AttendanceReportRequestModel request,
-  );
-
-  Future<Result<List<AttendanceActionReportModel>>> getActions(
-    AttendanceReportRequestModel request,
-  );
-
+  Future<Result<AttendanceReportModel>> getReport(AttendanceReportRequestModel request);
+  Future<Result<AttendanceReportModel>> getDailyReport({required DateTime date, int? employeeId, String? department});
+  Future<Result<AttendanceReportModel>> getLateReport(AttendanceReportRequestModel request);
+  Future<Result<AttendanceReportModel>> getEmployeeReport(int employeeId, DateTime fromDate, DateTime toDate);
+  Future<Result<List<AttendanceActionReportModel>>> getActions(AttendanceReportRequestModel request);
   Future<Result<List<int>>> getPdf(AttendanceReportRequestModel request);
-
-  Future<Result<List<int>>> getEmployeePdf(
-    int employeeId,
-    DateTime fromDate,
-    DateTime toDate,
-  );
+  Future<Result<List<int>>> getEmployeePdf(int employeeId, DateTime fromDate, DateTime toDate);
 }
