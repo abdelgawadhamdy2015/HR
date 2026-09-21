411d9044f7a2965032bac7ec91af368594a6eb7c
Abdelgawad Hamdy <34074927+abdelgawadhamdy2015@users.noreply.github.com>
2026-09-21 14:44:09 +0300

    Support saving user permission switches

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -12,6 +12,7 @@ class ApiConstants {
   static const String assignPermission = '/permissions/assign';
   static const String revokePermission = '/permissions/revoke';
   static String permissionsForUser(int userId) => '/permissions/user/$userId';
+  static String updateUserPermissions(int userId) => '/permissions/user/$userId';
   static const String users = '/users';
 
   static const String dashboardStats = '/dashboard/stats';
