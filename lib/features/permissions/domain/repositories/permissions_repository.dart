0c124a8faf897a7c26a29e7775588f805f47ccdb
Abdelgawad Hamdy <34074927+abdelgawadhamdy2015@users.noreply.github.com>
2026-09-21 16:15:18 +0300

    Expose users through permission repository

--- lib/features/permissions/domain/repositories/permissions_repository.dart
+++ lib/features/permissions/domain/repositories/permissions_repository.dart
@@ -1,9 +1,11 @@
 import '../../../../core/utils/result.dart';
 import '../entities/permission.dart';
+import '../entities/user_summary.dart';
 
 abstract class PermissionsRepository {
   Future<Result<List<Permission>>> getAll();
   Future<Result<List<Permission>>> getForUser(int userId);
+  Future<Result<List<UserSummary>>> getUsers();
   Future<Result<Permission>> create({required String name, String? description});
   Future<Result<void>> assign({required int userId, required int permissionId});
   Future<Result<void>> revoke({required int userId, required int permissionId});
